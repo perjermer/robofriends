f46bebc27536026285e5554136ffa12005fb8ec0
perjermer <pierce.morris1998@gmail.com>
2019-01-17 21:19:06 +0000

    Updates

---
+++ precache-manifest.8905f1bf0288510899865ee1f0e09e7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "1df58c76e181688d71ae",
+    "url": "/robofriends/static/js/main.1df58c76.chunk.js"
+  },
+  {
+    "revision": "2fd155b0a6a5e457ad4e",
+    "url": "/robofriends/static/js/1.2fd155b0.chunk.js"
+  },
+  {
+    "revision": "1df58c76e181688d71ae",
+    "url": "/robofriends/static/css/main.3f1a8d40.chunk.css"
+  },
+  {
+    "revision": "2fd155b0a6a5e457ad4e",
+    "url": "/robofriends/static/css/1.870bf2a9.chunk.css"
+  },
+  {
+    "revision": "e643232aeac0f243fcaa1a8626a4ccd4",
+    "url": "/robofriends/index.html"
+  }
+];
